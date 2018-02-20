09c332d25761b703bee90cb987e443db813990b2
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2018-02-20 17:54:35 +0100

    implemented the go-pg repos

--- storage/go-pg/template.go
+++ storage/go-pg/template.go
@@ -5,12 +5,38 @@ import (
 	"gopkg.in/pg.v5"
 )
 
-type templateRepository struct {
-	db *pg.DB
-}
-
 func NewTemplateRepository(db *pg.DB) communication.TemplateRepository {
 	return &templateRepository{
 		db: db,
 	}
+}
+
+type templateRepository struct {
+	db *pg.DB
+}
+
+func (repo *templateRepository) Get(id, locale string) (communication.Template, error) {
+	template := communication.Template{}
+
+	if err := repo.db.Model(&template).Where("template_id = ? AND locale = ?", id, locale).Select(); err != nil {
+		if err == pg.ErrNoRows {
+			return template, communication.TemplateNotFoundErr
+		}
+
+		return template, err
+	}
+
+	return template, nil
+}
+
+func (repo *templateRepository) Create(template *communication.Template) error {
+	return repo.db.Insert(template)
+}
+
+func (repo *templateRepository) Update(template *communication.Template) error {
+	return repo.db.Update(template)
+}
+
+func (repo *templateRepository) Delete(template *communication.Template) error {
+	return repo.db.Delete(template)
 }
--- storage/go-pg/transaction.go
+++ storage/go-pg/transaction.go
@@ -5,12 +5,34 @@ import (
 	"gopkg.in/pg.v5"
 )
 
-type transaction struct {
+func NewTransactionRepository(db *pg.DB) communication.TransactionRepository {
+	return &transactionRepo{
+		db: db,
+	}
+}
+
+type transactionRepo struct {
 	db *pg.DB
 }
 
-func NewTransactionRepository(db *pg.DB) communication.TransactionRepository {
-	return &transaction{
-		db: db,
+func (repo *transactionRepo) GetPending() ([]communication.Job, error) {
+	var jobs []communication.Job
+
+	if err := repo.db.Model(&jobs).Where("send_at is null").Select(); err != nil {
+		if err == pg.ErrNoRows {
+			return jobs, nil
+		}
+
+		return jobs, err
 	}
+
+	return jobs, nil
+}
+
+func (repo *transactionRepo) Create(job *communication.Job) error {
+	return repo.db.Insert(job)
+}
+
+func (repo *transactionRepo) Update(job *communication.Job) error {
+	return repo.db.Update(job)
 }
